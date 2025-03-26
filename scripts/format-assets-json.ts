49fabb56e09eb106eb6ca09665294cec176daefd
Jiankai Zeng <janpoem@163.com>
2025-03-27 07:55:03 +0800

    refactor: format assets

---
+++ scripts/format-assets-json.ts
@@ -0,0 +1,33 @@
+import { globSync } from 'glob';
+import { readFileSync, writeFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+const writeJsonFile = (item: string, path: string, data: unknown): void => {
+  try {
+    writeFileSync(path, JSON.stringify(data));
+    console.warn(`write file ${item}`);
+  } catch (err) {
+    console.warn(`parse ${item} json error: ${err}`);
+  }
+};
+
+const main = (items: string[]) => {
+  for (const item of items) {
+    const path = resolve(process.cwd(), item);
+    const text = readFileSync(path, 'utf8').toString();
+    try {
+      const json = JSON.parse(text);
+      writeJsonFile(item, path, json);
+    } catch (err) {
+      console.warn(`parse ${item} json error: ${err}`);
+    }
+  }
+};
+
+main(
+  globSync([
+    './packages/assets/locales/*.json',
+    './packages/assets/themes/*.json',
+    './packages/assets/tm/*.json',
+  ]),
+);
